0f7cacc31e47c1a4e31b226d232e47ec55939b24
RaulBecerraB <ingenierobecerrabarcelo@gmail.com>
2025-01-05 07:48:03 -0600

    Add base functional version

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,53 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Note from './components/Note'
 
-function App() {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
 
   return (
-    <>
-     
-    </>
+    <div className='m-6 p-4 bg-white shadow-md rounded-md shadow-black'>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note =>
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          className='border border-gray-300 rounded-md'
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">  save</button>
+      </form>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    important: true,
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    important: false,
+  },
+  {
+    id: 3,
+    content: 'Most important methods of HTTP-protocol are GET and POST',
+    important: true,
+  },
+]
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App notes={notes}/>
   </StrictMode>,
 )
